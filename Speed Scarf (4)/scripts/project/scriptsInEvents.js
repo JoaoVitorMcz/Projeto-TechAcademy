be1e62e1c988ec13201bcc87354784029d8033cf
João Vitor <joaovitormcz16@gmail.com>
2025-04-10 21:08:30 -0300

    adicionei um index do jogo para que ele fique mais bonito no site

---
+++ Speed Scarf (4)/scripts/project/scriptsInEvents.js
@@ -0,0 +1,17 @@
+
+
+const scriptsInEvents = {
+
+	async Floresta_Event14_Act3(runtime, localVars)
+	{
+		runtime.globalVars.Score += 10;
+		
+	},
+
+	async Floresta_Event19(runtime, localVars)
+	{
+		
+	}
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
